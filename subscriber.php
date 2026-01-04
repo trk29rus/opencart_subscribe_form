207ff4acf60f708913de563c84c5e1dc8f96883e
trk29rus <Avs29rus@mail.ru>
2026-01-05 02:22:53 +0300

    Delete subscriber.php

--- subscriber.php
+++
@@ -1,8 +0,0 @@
-<!--Разместить в /catalog/model/mail/-->
-<?php
-class ModelMailSubscribe extends Model {
-	public function add_subscriber($json) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "subscribe SET email = '".$json['email']."'");
-	
-	}
-}
