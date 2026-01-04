d4ac32872f13d70b3e98e0a94d190f83b45b002c
trk29rus <Avs29rus@mail.ru>
2026-01-05 02:22:33 +0300

    Update subscribe.php

--- catalog/model/mail/subscribe.php
+++ catalog/model/mail/subscribe.php
@@ -1,1 +1,7 @@
-
+<?php
+class ModelMailSubscribe extends Model {
+	public function add_subscriber($json) {
+		$this->db->query("INSERT INTO " . DB_PREFIX . "subscribe SET email = '".$json['email']."'");
+	
+	}
+}
