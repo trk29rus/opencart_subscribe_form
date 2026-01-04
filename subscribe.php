5fed685d40a696423a2fb46de8c046ab7844f6e3
trk29rus <Avs29rus@mail.ru>
2026-01-05 01:14:57 +0300

    Update subscribe.php

--- subscribe.php
+++ subscribe.php
@@ -7,23 +7,23 @@ class ControllerMailSubscribe extends Controller {
     $mail = new Mail($this->config->get('config_mail_engine'));
     $from = $this->config->get('config_email'); 
     $mail->parameter = $this->config->get('config_mail_parameter');
-		$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
-		$mail->smtp_username = $this->config->get('config_mail_smtp_username');
-		$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
-		$mail->smtp_port = $this->config->get('config_mail_smtp_port');
-		$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
-		$data = array();
-		foreach($json as $key => $val) {          
+	$mail->smtp_hostname = $this->config->get('config_mail_smtp_hostname');
+	$mail->smtp_username = $this->config->get('config_mail_smtp_username');
+	$mail->smtp_password = html_entity_decode($this->config->get('config_mail_smtp_password'), ENT_QUOTES, 'UTF-8');
+	$mail->smtp_port = $this->config->get('config_mail_smtp_port');
+	$mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
+	$data = array();
+	foreach($json as $key => $val) {          
           $data['subscriber'][] = array(
                'text' => $key,
                'value' => $val
 		   );
 		}     
-		$mail->setTo($from);
-		$mail->setFrom($from);
-		$mail->setSender('admin@your_store.ru');
-		$mail->setSubject('Новый подписчик на сайте');
-		$mail->setHtml($this->load->view('mail/subscribe', $data));
-		$mail->send();		
+	$mail->setTo($from);
+	$mail->setFrom($from);
+	$mail->setSender('admin@your_store.ru');
+	$mail->setSubject('Новый подписчик на сайте');
+	$mail->setHtml($this->load->view('mail/subscribe', $data));
+	$mail->send();		
     }    
 }
